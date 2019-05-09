5042be27719bfd4803ad628c6fb1f3a18c089778
Grahame Grieve <grahameg@gmail.com>
2019-05-10 01:19:14 +1000

    Release new version

--- release.bat
+++ release.bat
@@ -4,17 +4,17 @@ REM make sure you are committed
 echo
 
 echo ===========================================================================
-echo upgrade and release fhir IG Publisher from 0.9.4-SNAPSHOT to 0.9.5-SNAPSHOT
+echo upgrade and release fhir IG Publisher from 0.9.5-SNAPSHOT to 0.9.6-SNAPSHOT
 echo ===========================================================================
 pause
 
-call mvn versions:set -DnewVersion=0.9.5-SNAPSHOT
+call mvn versions:set -DnewVersion=0.9.6-SNAPSHOT
 call git commit -a -m "Release new version"
 call git push origin master
-call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --find "0.9.4-SNAPSHOT" --replace "0.9.5-SNAPSHOT"
+call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --find "0.9.5-SNAPSHOT" --replace "0.9.6-SNAPSHOT"
 call mvn deploy
-copy org.hl7.fhir.publisher.cli\target\org.hl7.fhir.publisher.cli-0.9.5-SNAPSHOT.jar ..\igpublisher.jar
-call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java IGPublisher" -m "New Java IGPublisher v0.9.5-SNAPSHOT released." --config-file zuliprc
+copy org.hl7.fhir.publisher.cli\target\org.hl7.fhir.publisher.cli-0.9.6-SNAPSHOT.jar ..\igpublisher.jar
+call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java IGPublisher" -m "New Java IGPublisher v0.9.6-SNAPSHOT released." --config-file zuliprc
 
 echo ===============================================================
 echo all done
