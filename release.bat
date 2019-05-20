25c4739dd06ced43f1e18d7bc20fb69838309be4
Grahame Grieve <grahameg@gmail.com>
2019-05-20 21:23:52 +1000

    Release new version

--- release.bat
+++ release.bat
@@ -2,10 +2,11 @@ REM replace versions before running
 REM make sure you are committed
 
 echo
-
 echo ===========================================================================
 echo upgrade and release fhir IG Publisher from 0.9.9-SNAPSHOT to 0.9.10-SNAPSHOT
 echo ===========================================================================
+echo
+echo Make sure code is commmitted and check the versions...
 pause
 
 call mvn versions:set -DnewVersion=0.9.10-SNAPSHOT
