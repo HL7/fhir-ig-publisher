fd14fc34b6dc3e6d1f2d1d1bd2f4fac35e4804af
Grahame Grieve <grahameg@gmail.com>
2019-05-21 11:23:46 +1000

    Release new version

--- release.bat
+++ release.bat
@@ -1,21 +1,22 @@
 REM replace versions before running
 REM make sure you are committed
 
-echo
+@echo off
+echo ..
 echo ===========================================================================
-echo upgrade and release fhir IG Publisher from 0.9.9-SNAPSHOT to 0.9.10-SNAPSHOT
+echo upgrade and release fhir IG Publisher from 0.9.10-SNAPSHOT to 0.9.11-SNAPSHOT
 echo ===========================================================================
-echo
+echo ..
 echo Make sure code is commmitted and check the versions...
 pause
 
-call mvn versions:set -DnewVersion=0.9.10-SNAPSHOT
+call mvn versions:set -DnewVersion=0.9.11-SNAPSHOT
 call git commit -a -m "Release new version"
 call git push origin master
-call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --find "0.9.9-SNAPSHOT" --replace "0.9.10-SNAPSHOT"
+call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --find "0.9.10-SNAPSHOT" --replace "0.9.11-SNAPSHOT"
 call mvn deploy
-copy org.hl7.fhir.publisher.cli\target\org.hl7.fhir.publisher.cli-0.9.10-SNAPSHOT.jar ..\igpublisher.jar
-call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java IGPublisher" -m "New Java IGPublisher v0.9.10-SNAPSHOT released at https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=org.hl7.fhir.publisher&a=org.hl7.fhir.publisher.cli&v=0.9.10-SNAPSHOT&e=jar" --config-file zuliprc
+copy org.hl7.fhir.publisher.cli\target\org.hl7.fhir.publisher.cli-0.9.11-SNAPSHOT.jar ..\igpublisher.jar
+call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java IGPublisher" -m "New Java IGPublisher v0.9.11-SNAPSHOT released at https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=org.hl7.fhir.publisher&a=org.hl7.fhir.publisher.cli&v=0.9.11-SNAPSHOT&e=jar" --config-file zuliprc
 
 echo ===============================================================
 echo all done
