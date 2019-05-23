fee2f1fd8ae377578043aa5622a323dfb150e523
Grahame Grieve <grahameg@gmail.com>
2019-05-23 15:02:47 +1000

    update for new core

--- release.bat
+++ release.bat
@@ -3,23 +3,27 @@ REM make sure you are committed
 
 @echo off
 echo ..
-echo ===========================================================================
+echo =============================================================================
 echo upgrade and release fhir IG Publisher from 0.9.10-SNAPSHOT to 0.9.11-SNAPSHOT
-echo ===========================================================================
+echo =============================================================================
 echo ..
 echo Make sure code is commmitted and check the versions...
 pause
 
 call mvn versions:set -DnewVersion=0.9.11-SNAPSHOT
-call git commit -a -m "Release new version"
+call git commit -a -m "Release new version 0.9.11-SNAPSHOT"
 call git push origin master
 call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --find "0.9.10-SNAPSHOT" --replace "0.9.11-SNAPSHOT"
 call mvn deploy
-copy org.hl7.fhir.publisher.cli\target\org.hl7.fhir.publisher.cli-0.9.11-SNAPSHOT.jar ..\igpublisher.jar
-call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java IGPublisher" -m "New Java IGPublisher v0.9.11-SNAPSHOT released at https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=org.hl7.fhir.publisher&a=org.hl7.fhir.publisher.cli&v=0.9.11-SNAPSHOT&e=jar" --config-file zuliprc
+copy org.hl7.fhir.publisher.cli\target\org.hl7.fhir.publisher.cli-0.9.11-SNAPSHOT.jar ..\latest-ig-publisher\org.hl7.fhir.ig-publisher.jar
+cd ..\latest-ig-publisher
+call git commit -a -m "Release new version 0.9.11-SNAPSHOT"
+call git push origin master
+cd ..\fhir-ig-publisher
+call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java IGPublisher" -m "New Java IGPublisher v0.9.11-SNAPSHOT released at https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=org.hl7.fhir.publisher&a=org.hl7.fhir.publisher.cli&v=0.9.11-SNAPSHOT&e=jar, and also deployed at https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.ig-publisher.jar" --config-file zuliprc
 
-echo ===============================================================
+echo ========
 echo all done
-echo ===============================================================
+echo ========
 pause
  
