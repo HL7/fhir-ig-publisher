bf9a4aab752bcfbb9d70672dbce26007e0dc0e3a
Grahame Grieve <grahameg@gmail.com>
2019-10-07 12:01:11 +1100

    add missing file

---
+++ org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/renderers/OperationDefinitionRenderer.java
@@ -0,0 +1,121 @@
+package org.hl7.fhir.igtools.renderers;
+
+/*-
+ * #%L
+ * org.hl7.fhir.publisher.core
+ * %%
+ * Copyright (C) 2014 - 2019 Health Level 7
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.igtools.publisher.FetchedFile;
+import org.hl7.fhir.igtools.publisher.FetchedResource;
+import org.hl7.fhir.igtools.publisher.IGKnowledgeProvider;
+import org.hl7.fhir.igtools.publisher.SpecMapManager;
+import org.hl7.fhir.r4.model.UrlType;
+import org.hl7.fhir.r5.conformance.ProfileUtilities;
+import org.hl7.fhir.r5.conformance.ProfileUtilities.ProfileKnowledgeProvider.BindingResolution;
+import org.hl7.fhir.r5.context.IWorkerContext;
+import org.hl7.fhir.r5.elementmodel.Element;
+import org.hl7.fhir.r5.formats.IParser.OutputStyle;
+import org.hl7.fhir.r5.formats.XmlParser;
+import org.hl7.fhir.r5.model.CanonicalType;
+import org.hl7.fhir.r5.model.CodeSystem;
+import org.hl7.fhir.r5.model.CodeableConcept;
+import org.hl7.fhir.r5.model.Coding;
+import org.hl7.fhir.r5.model.ElementDefinition;
+import org.hl7.fhir.r5.model.ElementDefinition.ElementDefinitionBindingComponent;
+import org.hl7.fhir.r5.model.ElementDefinition.ElementDefinitionConstraintComponent;
+import org.hl7.fhir.r5.model.ElementDefinition.ElementDefinitionExampleComponent;
+import org.hl7.fhir.r5.model.ElementDefinition.ElementDefinitionMappingComponent;
+import org.hl7.fhir.r5.model.ElementDefinition.ElementDefinitionSlicingComponent;
+import org.hl7.fhir.r5.model.ElementDefinition.ElementDefinitionSlicingDiscriminatorComponent;
+import org.hl7.fhir.r5.model.ElementDefinition.SlicingRules;
+import org.hl7.fhir.r5.model.ElementDefinition.TypeRefComponent;
+import org.hl7.fhir.r5.model.Enumerations.BindingStrength;
+import org.hl7.fhir.r5.model.Extension;
+import org.hl7.fhir.r5.model.IdType;
+import org.hl7.fhir.r5.model.OperationDefinition;
+import org.hl7.fhir.r5.model.PrimitiveType;
+import org.hl7.fhir.r5.model.Quantity;
+import org.hl7.fhir.r5.model.StringType;
+import org.hl7.fhir.r5.model.StructureDefinition;
+import org.hl7.fhir.r5.model.StructureDefinition.StructureDefinitionKind;
+import org.hl7.fhir.r5.model.StructureDefinition.StructureDefinitionMappingComponent;
+import org.hl7.fhir.r5.model.StructureDefinition.TypeDerivationRule;
+import org.hl7.fhir.r5.model.Type;
+import org.hl7.fhir.r5.model.ValueSet;
+import org.hl7.fhir.r5.utils.ElementDefinitionUtilities;
+import org.hl7.fhir.r5.utils.ToolingExtensions;
+import org.hl7.fhir.utilities.CommaSeparatedStringBuilder;
+import org.hl7.fhir.utilities.MarkDownProcessor;
+import org.hl7.fhir.utilities.Utilities;
+import org.hl7.fhir.utilities.cache.NpmPackage;
+import org.hl7.fhir.utilities.xhtml.XhtmlComposer;
+import org.hl7.fhir.utilities.xhtml.HierarchicalTableGenerator.Piece;
+
+public class OperationDefinitionRenderer extends BaseRenderer {
+  public static final String RIM_MAPPING = "http://hl7.org/v3";
+  public static final String v2_MAPPING = "http://hl7.org/v2";
+  public static final String LOINC_MAPPING = "http://loinc.org";
+  public static final String SNOMED_MAPPING = "http://snomed.info";
+
+  ProfileUtilities utils;
+  private OperationDefinition od;
+  private String destDir;
+  private List<FetchedFile> files;
+
+  public OperationDefinitionRenderer(IWorkerContext context, String prefix, OperationDefinition od, String destDir, IGKnowledgeProvider igp, List<SpecMapManager> maps, MarkDownProcessor markdownEngine, NpmPackage packge, List<FetchedFile> files) {
+    super(context, prefix, igp, maps, markdownEngine, packge);
+    this.od = od;
+    this.destDir = destDir;
+    utils = new ProfileUtilities(context, null, igp);
+    utils.setIgmode(true);
+    this.files = files;
+  }
+
+  @Override
+  public void setTranslator(org.hl7.fhir.utilities.TranslationServices translator) {
+    super.setTranslator(translator);
+    utils.setTranslator(translator);
+  }
+
+  public String summary() {
+    return "<p><i>Not Done Yet</i></p>";
+  }
+
+  public String idempotence() {
+    if (!od.hasAffectsStateElement())
+      return "";
+    if (!od.getAffectsState())
+      return "<p>This operation may or does change the state of resources on the server (not idempotent)</p>";  
+    else  
+      return "<p>This operation does not change the state of resources on the server (idempotent)</p>";  
+  }
+
+}
