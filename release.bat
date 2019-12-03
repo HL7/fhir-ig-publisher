d0d7037eb12ccfc64571df038a374673c2cd1253
Grahame Grieve <grahameg@gmail.com>
2019-12-03 23:10:50 +1100

    Release new version 1.0.6-SNAPSHOT

--- release.bat
+++ release.bat
@@ -13,6 +13,9 @@ echo Make sure code is commmitted and check the versions...
 pause
 
 call mvn versions:set -DnewVersion=%newver%-SNAPSHOT
+
+pause
+
 call git commit -a -m "Release new version %newver%-SNAPSHOT"
 call git push origin master
 call "C:\tools\fnr.exe" -dir "C:\work\org.hl7.fhir\build" -fileMask "*.xml" -find "%oldver%-SNAPSHOT" -replace "%newver%-SNAPSHOT" -count 0
