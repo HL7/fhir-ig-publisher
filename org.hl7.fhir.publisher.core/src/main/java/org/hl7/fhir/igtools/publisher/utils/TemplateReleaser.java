665029ca279e5fcd47a3a65bc1ac844392435677
Grahame Grieve <grahameg@gmail.com>
2020-02-17 22:25:55 +1100

    set up template release process

--- org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/utils/TemplateReleaser.java
+++ org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/utils/TemplateReleaser.java
@@ -1,16 +1,25 @@
 package org.hl7.fhir.igtools.publisher.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -22,7 +31,15 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.hl7.fhir.igtools.publisher.IGVersionUtil;
+import org.hl7.fhir.igtools.publisher.Publisher;
+import org.hl7.fhir.igtools.publisher.Publisher.CacheOption;
+import org.hl7.fhir.igtools.publisher.Publisher.IGBuildMode;
+import org.hl7.fhir.igtools.publisher.utils.TemplateReleaser.VersionDecision;
+import org.hl7.fhir.utilities.IniFile;
+import org.hl7.fhir.utilities.TextFile;
 import org.hl7.fhir.utilities.Utilities;
+import org.hl7.fhir.utilities.VersionUtilities;
 import org.hl7.fhir.utilities.cache.NpmPackage;
 import org.hl7.fhir.utilities.json.JSONUtil;
 import org.hl7.fhir.utilities.json.JsonTrackingParser;
@@ -31,10 +48,79 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class TemplateReleaser {
   
+  public enum VersionChangeType {
+    NONE, PATCH, MINOR, MAJOR;
+
+    public boolean lessThan(VersionChangeType t) {
+      switch (this) {
+      case MAJOR: return false;
+      case MINOR: return (t == MAJOR);
+      case NONE: return t != NONE; 
+      case PATCH: return (t == MAJOR || t == MINOR);
+      }
+      return false;
+    }
+  }
+
+  public class VersionDecision {
+    private String id;
+    private String currentVersion;
+    private String newVersion;
+    private VersionChangeType type;
+    private boolean explicit;
+    private String implicitSource;
+    private String releaseNote;
+    private Boolean checked = null;
+    private boolean built;
+    public String getId() {
+      return id;
+    }
+    public void setId(String id) {
+      this.id = id;
+    }
+
+
+    public String getCurrentVersion() {
+      return currentVersion;
+    }
+    public void setCurrentVersion(String currentVersion) {
+      this.currentVersion = currentVersion;
+    }
+    public String getNewVersion() {
+      return newVersion;
+    }
+    public void setNewVersion(String newVersion) {
+      this.newVersion = newVersion;
+    }
+    public VersionChangeType getType() {
+      return type;
+    }
+    public void setType(VersionChangeType type) {
+      this.type = type;
+    }
+    public boolean isExplicit() {
+      return explicit;
+    }
+    public void setExplicit(boolean explicit) {
+      this.explicit = explicit;
+    }
+    public String summary() {
+      if (type == VersionChangeType.NONE) {
+        return id+"#"+currentVersion+" (no change)";        
+      } else {
+        return id+"#"+currentVersion+" ->: "+newVersion+" "+(explicit ? "" : "(implied by "+implicitSource+") ");
+      }
+    }
+  }
+
   private static final String RSS_DATE = "EEE, dd MMM yyyy hh:mm:ss";
 
   private Document rss;
@@ -46,25 +132,314 @@ public class TemplateReleaser {
 
   // 3 parameters: source of package, package dest folder, and release note
   public static void main(String[] args) throws Exception {
-    new TemplateReleaser().release(args[0], args[1], args[2]);
+    try {
+      new TemplateReleaser().release(args[0], args[1]);
+    } catch (Throwable e) {
+      System.out.println("Error releasing templates from "+args[0]+" to "+args[1]+":");
+      System.out.println(e.getMessage());
+      System.out.println("");
+      e.printStackTrace();
+    }
   }
 
-  private void release(String source, String dest, String note) throws Exception {
+  private void release(String source, String dest) throws Exception {
     SimpleDateFormat df = new SimpleDateFormat(RSS_DATE, new Locale("en", "US"));
     checkDest(dest);
-    for (File f : new File(source).listFiles()) {
-      if (f.isDirectory()) {
-        release(source, dest, f.getName(), note, df);
+    Map<String, String> currentPublishedVersions = scanForCurrentVersions(dest);
+    System.out.println("Current Published Versions");
+    for (String s : currentPublishedVersions.keySet()) {
+      System.out.println(" * "+s+"#"+currentPublishedVersions.get(s));
+    }
+    List<VersionDecision> versionsList = analyseVersions(source, scanForCurrentVersions(source), currentPublishedVersions);
+    System.out.println("Actions to take");
+    for (VersionDecision vd : versionsList) {
+      System.out.println(" * "+vd.summary());
+    }
+    System.out.println("Do you want to continue [y/n]");
+    int r = System.in.read();
+    if (r == 'y') {
+      // now: for any implicit upgrades, set up the package-list.json
+      System.out.println("Updating Package Lists");
+      for (VersionDecision vd : versionsList) {
+        if (!vd.explicit && vd.type != VersionChangeType.NONE) {
+          updatePackageList(source, vd);
+        }
+      }
+      SimpleDateFormat dfd = new SimpleDateFormat("yyyy-MM-dd", new Locale("en", "US"));
+      String dateFmt = dfd.format(new Date());
+      for (VersionDecision vd : versionsList) {
+        if (vd.type != VersionChangeType.NONE) {
+          updateDate(source, vd, dateFmt);
+        }
+      }
+      System.out.println("Updating Packages");
+      for (VersionDecision vd : versionsList) {
+        updateVersions(source, vd, versionsList);
+      }
+      System.out.println("Building IGs");
+      for (VersionDecision vd : versionsList) {
+        if (!vd.built) {
+          build(source, vd, versionsList);
+        }
+      }
+      System.out.println("Finished Building IGs");
+    
+      System.out.println("Releasing Packages");
+      for (VersionDecision vd : versionsList) {
+        if (vd.type != VersionChangeType.NONE) {
+          release(dest, source, vd, df);
+        }
+      }
+
+      System.out.println("Reset Packages");
+      for (VersionDecision vd : versionsList) {
+        resetVersions(source, vd, versionsList);
+      }
+      Element lbd = XMLUtil.getNamedChild(channel, "lastBuildDate");
+      lbd.setTextContent(df.format(new Date()));
+      File bak = new File(Utilities.changeFileExt(xml.getAbsolutePath(),  ".bak"));
+      if (bak.exists())
+        bak.delete();
+      xml.renameTo(bak);
+      saveXml(new FileOutputStream(xml));  
+      System.out.println("Published");
+    }
+  }
+
+  private void build(String source, VersionDecision vd, List<VersionDecision> versions) throws Exception {
+    List<String> dependendencies = listDependencies(source, vd.getId());
+    for (String s : dependendencies) {
+      VersionDecision v = findVersion(versions, s);
+      if (!v.built) {
+        build(source, v, versions);
       }
     }
-    Element lbd = XMLUtil.getNamedChild(channel, "lastBuildDate");
-    lbd.setTextContent(df.format(new Date()));
-    File bak = new File(Utilities.changeFileExt(xml.getAbsolutePath(),  ".bak"));
-    if (bak.exists())
-      bak.delete();
-    xml.renameTo(bak);
-    saveXml(new FileOutputStream(xml));  
-    System.out.println("Published");
+    Publisher.publishDirect(Utilities.path(source, vd.getId()));
+  }
+
+  private void updateVersions(String source, VersionDecision vd, List<VersionDecision> versionsList) throws FileNotFoundException, IOException {
+    JsonObject npm = JsonTrackingParser.parseJson(new FileInputStream(Utilities.path(source, vd.getId(), "package", "package.json")));
+    npm.remove("version");
+    npm.addProperty("version", vd.getNewVersion());
+    if (npm.has("dependencies")) {
+      JsonObject d = npm.getAsJsonObject("dependencies");
+      List<String> deps = new ArrayList<>();
+      for (Entry<String, JsonElement> e : d.entrySet()) {
+        deps.add(e.getKey());
+      }
+      for (String s : deps) {
+        d.remove(s);
+        d.addProperty(s, findVersion(versionsList, s).newVersion);
+      }
+    }
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    String jcnt = gson.toJson(npm);
+    TextFile.stringToFile(jcnt, Utilities.path(source, vd.getId(), "package", "package.json"));
+  }
+
+  private void resetVersions(String source, VersionDecision vd, List<VersionDecision> versionsList) throws FileNotFoundException, IOException {
+    JsonObject npm = JsonTrackingParser.parseJson(new FileInputStream(Utilities.path(source, vd.getId(), "package", "package.json")));
+    if (npm.has("dependencies")) {
+      JsonObject d = npm.getAsJsonObject("dependencies");
+      List<String> deps = new ArrayList<>();
+      for (Entry<String, JsonElement> e : d.entrySet()) {
+        deps.add(e.getKey());
+      }
+      for (String s : deps) {
+        d.remove(s);
+        d.addProperty(s, "current");
+      }
+      Gson gson = new GsonBuilder().setPrettyPrinting().create();
+      String jcnt = gson.toJson(npm);
+      TextFile.stringToFile(jcnt, Utilities.path(source, vd.getId(), "package", "package.json"));
+    }
+  }
+
+  private void updateDate(String source, VersionDecision vd, String dateFmt) throws FileNotFoundException, IOException {
+    JsonObject pl = JsonTrackingParser.parseJson(new FileInputStream(Utilities.path(source, vd.getId(), "package-list.json")));
+    boolean ok = false;
+    for (JsonObject v : JSONUtil.objects(pl, "list")) {
+      if (JSONUtil.str(v, "version").equals(vd.getNewVersion())) {
+        v.remove("date");
+        v.addProperty("date", dateFmt);
+        ok = true;
+        vd.releaseNote = JSONUtil.str(v, "desc");
+      }
+    }
+    if (!ok) {
+      throw new Error("unable to find version "+vd.getNewVersion()+" in pacjage list");
+    }
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    String jcnt = gson.toJson(pl);
+    TextFile.stringToFile(jcnt, Utilities.path(source, vd.getId(), "package-list.json"));
+  }
+
+  private void updatePackageList(String source, VersionDecision vd) throws FileNotFoundException, IOException {
+    JsonObject pl = JsonTrackingParser.parseJson(new FileInputStream(Utilities.path(source, vd.getId(), "package-list.json")));
+    JsonArray vl = pl.getAsJsonArray("list");
+    JsonArray nvl = new JsonArray();
+    JsonObject v = new JsonObject();
+    nvl.add(vl.get(0));
+    nvl.add(v);
+    for (int i = 1; i < vl.size(); i++) {
+      JsonObject vo = (JsonObject) vl.get(i);
+      if (vo.has("current")) {
+        vo.remove("current");
+      }
+      nvl.add(vo);
+    }
+    pl.remove("list");
+    pl.add("list", nvl);
+    v.addProperty("version", vd.newVersion);
+    v.addProperty("date", "XXXX-XX-XX");
+    v.addProperty("desc", "Upgrade for dependency on "+vd.implicitSource);
+    v.addProperty("path", Utilities.pathURL(pl.get("canonical").getAsString(), vd.newVersion));
+    v.addProperty("status", "release");
+    v.addProperty("sequence", "Publications");
+    v.addProperty("current", true);
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    String jcnt = gson.toJson(pl);
+    TextFile.stringToFile(jcnt, Utilities.path(source, vd.getId(), "package-list.json"));
+  }
+
+  private List<VersionDecision> analyseVersions(String source, Map<String, String> newList, Map<String, String> oldList) throws Exception {
+    List<VersionDecision> res = new ArrayList<TemplateReleaser.VersionDecision>();
+    for (String s : oldList.keySet()) {
+      if (!newList.containsKey(s)) {
+        throw new Exception("Existing template "+s+" not found in release set");
+      }
+      VersionDecision vd = new VersionDecision();
+      res.add(vd);
+      vd.setId(s);
+      String v = oldList.get(s);
+      String nv = newList.get(s);
+      vd.setCurrentVersion(v);
+      vd.setType(checkVersionChangeType(v, nv));
+      vd.setExplicit(vd.getType() != VersionChangeType.NONE);
+      if (vd.isExplicit()) {
+        vd.setNewVersion(nv);
+      }
+    }
+    for (String s : newList.keySet()) {
+      if (!oldList.containsKey(s)) {
+        throw new Exception("New template "+s+" not found in existing set - manual set up required");
+      }
+    }
+    // ok, now check for implied upgrades
+    for (VersionDecision vd : res) {
+      if (vd.checked == null) {
+        checkDependencies(source, vd, res);
+      }
+    }
+    // now execute implied upgrades
+    for (VersionDecision vd : res) {
+      if (!vd.isExplicit()) {
+        vd.setNewVersion(getNewVersion(vd.getType(), vd.getCurrentVersion()));
+      }
+    }
+    boolean any = false;
+    for (VersionDecision vd : res) {
+      any = any || vd.type != VersionChangeType.NONE;
+    }
+    if (!any) {
+      throw new Exception("Nothing found to release - Cannot Proceed");
+    }
+    
+    return res;
+  }
+
+  private String getNewVersion(VersionChangeType t, String v) {
+    switch (t) {
+    case MAJOR: return VersionUtilities.incMajorVersion(v);
+    case MINOR: return VersionUtilities.incMinorVersion(v);
+    case PATCH: return VersionUtilities.incPatchVersion(v);
+    }
+    return v;
+  }
+  
+  private void checkDependencies(String source, VersionDecision vd, List<VersionDecision> versions) throws Exception {
+    vd.checked = false;
+    List<String> dependendencies = listDependencies(source, vd.getId());
+    for (String s : dependendencies) {
+      VersionDecision v = findVersion(versions, s);
+      if (v.checked == null) {
+        checkDependencies(source, v, versions);
+      } else if (!v.checked) {
+        throw new Exception("Circular dependency");
+      }
+      VersionChangeType t = v.type;
+      if (vd.isExplicit()) {
+        if (vd.type.lessThan(t)) {
+          throw new Exception("invalid operation");
+        }
+      } else {
+        if (vd.type.lessThan(t)) {
+          vd.type = t;
+          if (v.explicit) {
+            vd.implicitSource = v.id;
+          } else {
+            vd.implicitSource = v.implicitSource;
+          }
+        }
+      }
+    }
+    vd.checked = true;
+  }
+
+  private VersionDecision findVersion(List<VersionDecision> versions, String s) {
+    for (VersionDecision v : versions) {
+      if (v.id.equals(s)) {
+        return v;
+      }
+    }
+    return null;
+  }
+
+  private List<String> listDependencies(String source, String id) throws Exception {
+    JsonObject npm = JsonTrackingParser.parseJson(TextFile.fileToString(Utilities.path(source, id, "package", "package.json")));
+    List<String> res = new ArrayList<String>();
+    if (npm.has("dependencies")) {
+      for (Entry<String, JsonElement> s : npm.getAsJsonObject("dependencies").entrySet()) {
+//        if (!"current".equals(s.getValue().getAsString())) {
+//          throw new Exception("Dependency is not 'current'");
+//        }
+        res.add(s.getKey());
+      }
+    }
+    return res;
+  }
+
+  private VersionChangeType checkVersionChangeType(String v, String nv) {
+    if (v.equals(nv)) { 
+      return VersionChangeType.NONE;
+    } else if (VersionUtilities.versionsCompatible(v, nv)) {
+      return VersionChangeType.PATCH;
+    } else if (v.charAt(0) == nv.charAt(0)) {
+      return VersionChangeType.MINOR;
+    } else {
+      return VersionChangeType.MAJOR;
+    }
+  }
+
+  private Map<String, String> scanForCurrentVersions(String folder) throws IOException {
+    Map<String, String> res = new HashMap<String, String>();
+    scanForCurrentVersions(res, new File(folder));
+    return res;
+  }
+
+  private void scanForCurrentVersions(Map<String, String> res, File folder) throws IOException {
+    for (File f : folder.listFiles()) {
+      if (f.isDirectory()) {
+        scanForCurrentVersions(res, f);
+      } else if (f.getName().equals("package-list.json")) {
+        JsonObject pl = JsonTrackingParser.parseJson(f);
+        for (JsonObject v : JSONUtil.objects(pl, "list")) {
+          if ("release".equals(JSONUtil.str(v, "status")) && JSONUtil.bool(v, "current")) {
+            res.put(JSONUtil.str(pl, "package-id"), JSONUtil.str(v, "version"));
+          }
+        }
+      }
+    }
   }
 
   private void checkDest(String dest) throws Exception {
@@ -103,23 +478,36 @@ public class TemplateReleaser {
   }
     
 
-  private void release(String source, String dest, String folder, String note, SimpleDateFormat df) throws Exception {
+  private void release(String dest, String source, VersionDecision vd, SimpleDateFormat df) throws Exception {
 
-    NpmPackage npm = checkPackage(source, folder);
+    NpmPackage npm = checkPackage(source, vd.id);
 
     boolean isNew = true;
     for (Element item : XMLUtil.getNamedChildren(channel, "item")) {
       isNew = isNew && !(npm.name()+"#"+npm.version()).equals(XMLUtil.getNamedChildText(item, "title"));
     }
     if (isNew) {
-      System.out.println("Publish Package at "+source+"\\"+folder+" to "+dest+". release note = "+note);
-      checkNote(note);
+      System.out.println("Publish Package at "+source+"\\"+vd.id+" to "+dest+". release note = "+vd.releaseNote);
+      checkNote(vd.releaseNote);
 
       File dst = new File(Utilities.path(dest, npm.name(), npm.version()));
       check(!dst.exists(), "Implied destination "+dst.getAbsolutePath()+" already exists - check that a new version is being released.");  
     
+      // if jekyll.html exists, delete index.html and rename jekyll.html to index.html
+      File src = new File(Utilities.path(source, vd.getId(), "output"));
+      File jf = new File(Utilities.path(source, vd.getId(), "output", "jekyll.html"));
+      File xf = new File(Utilities.path(source, vd.getId(), "output", "index.html"));
+      if (jf.exists()) {
+        xf.delete();
+        jf.renameTo(xf);
+      }
+      // copy files
       Utilities.createDirectory(dst.getAbsolutePath());
-      npm.save(new FileOutputStream(Utilities.path(dst.getAbsolutePath(), "package.tgz")));
+      Utilities.copyDirectory(Utilities.path(source, vd.getId(), "output"), Utilities.path(dest, npm.name(), npm.version()), null);
+      Utilities.copyDirectory(Utilities.path(source, vd.getId(), "output"), Utilities.path(dest, npm.name()), null);
+      Utilities.copyFile(Utilities.path(source, vd.getId(), "package-list.json"), Utilities.path(dest, npm.name(), "package-list.json"));
+      
+      // update rss feed      
       Element item = rss.createElement("item");
       List<Element> list = XMLUtil.getNamedChildren(channel, "item");
       Node txt = rss.createTextNode("\n    ");
@@ -130,7 +518,7 @@ public class TemplateReleaser {
       }
       channel.insertBefore(item, txt);
       addTextChild(item, "title", npm.name()+"#"+npm.version());
-      addTextChild(item, "description", note);
+      addTextChild(item, "description", vd.releaseNote);
       addTextChild(item, "link", Utilities.pathURL(linkRoot, npm.name(), npm.version(), "package.tgz"));
       addTextChild(item, "guid", Utilities.pathURL(linkRoot, npm.name(), npm.version(), "package.tgz")).setAttribute("isPermaLink", "true");
       addTextChild(item, "dc:creator", "FHIR Project");
