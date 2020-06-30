983d304129854589fa921732fc38ecddd0ad37e1
markiantorno <markiantorno@gmail.com>
2020-06-30 10:57:06 -0400

    cleanup

--- org.hl7.fhir.publisher.cli/src/main/java/Placeholder.java
+++ org.hl7.fhir.publisher.cli/src/main/java/Placeholder.java
@@ -5,10 +5,4 @@ import org.hl7.fhir.utilities.Utilities;
  * to be created so that Maven Central doesn't complain.
  */
 public class Placeholder {
-	private void psvm() {
-//		// TODO Auto-generated method stub
-//		Utilities.bang("poop", true);
-//		Utilities.dump(true, 23);
-//		Utilities.blah();
-	}
 }
