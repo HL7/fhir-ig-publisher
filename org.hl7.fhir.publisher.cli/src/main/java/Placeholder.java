5678da91bd3bbcf94fddbd90065f6307451d90e2
markiantorno <markiantorno@gmail.com>
2020-06-29 20:04:27 -0400

    build changes for pipelines

--- org.hl7.fhir.publisher.cli/src/main/java/Placeholder.java
+++ org.hl7.fhir.publisher.cli/src/main/java/Placeholder.java
@@ -1,6 +1,14 @@
+import org.hl7.fhir.utilities.Utilities;
+
 /**
  * This class is just a placeholder to force the source JAR and javadoc JAR
  * to be created so that Maven Central doesn't complain.
  */
 public class Placeholder {
+	private void psvm() {
+//		// TODO Auto-generated method stub
+//		Utilities.bang("poop", true);
+//		Utilities.dump(true, 23);
+//		Utilities.blah();
+	}
 }
