913ca2fa6b916112750b8e74db7d73378b583cef
dotasek <david.otasek@smilecdr.com>
2023-03-13 16:41:39 -0400

    Mock out git creation

--- org.hl7.fhir.publisher.core/src/test/java/org/hl7/fhir/igtools/publisher/GitUtilitiesTests.java
+++ org.hl7.fhir.publisher.core/src/test/java/org/hl7/fhir/igtools/publisher/GitUtilitiesTests.java
@@ -1,25 +1,53 @@
 package org.hl7.fhir.igtools.publisher;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.io.File;
 import java.io.IOException;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import static org.hl7.fhir.igtools.publisher.GitUtilities.execAndReturnString;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GitUtilitiesTests {
+
+	File normalBranchDirectory;
+	File worktreeBranchDirectory;
+	@BeforeAll
+	public void beforeAll() throws IOException, InterruptedException {
+		File gitRoot = Files.createTempDirectory("testGitDirectory").toFile();
+
+		normalBranchDirectory = Path.of(gitRoot.getAbsolutePath(),"normal-branch").toFile();
+		normalBranchDirectory.mkdir();
+
+		Path worktreeBranchPath = Path.of(gitRoot.getAbsolutePath(), "branch-a");
+
+		System.out.println(execAndReturnString(new String[]{"git", "init"}, null, normalBranchDirectory));
+		System.out.println(execAndReturnString(new String[]{"touch", "dummy.txt"}, null, normalBranchDirectory));
+
+		System.out.println(execAndReturnString(new String[]{"git", "add", "./dummy.txt"},null, normalBranchDirectory));
+		System.out.println(execAndReturnString(new String[]{"git", "commit", "-m", "test"},null, normalBranchDirectory));
+		System.out.println(execAndReturnString(new String[]{"git", "branch", "branch-a"}, null, normalBranchDirectory));
+		System.out.println(execAndReturnString(new String[]{"git", "worktree", "add", worktreeBranchPath.toString(), "branch-a"}, null, normalBranchDirectory));
+
+		worktreeBranchDirectory = worktreeBranchPath.toFile();
+	}
 	@Test
 	public void testGetGitStatus() {
-		String output = GitUtilities.getGitStatus(new File("src/test/resources/git/regular-branch/"));
+		String output = GitUtilities.getGitStatus(normalBranchDirectory);
 		assertEquals("main", output.trim());
 	}
 
 	@Test
 	public void testGetGitWorktreeStatus() {
-		String output = GitUtilities.getGitStatus(new File("src/test/resources/git/worktree-branch/"));
+		String output = GitUtilities.getGitStatus(worktreeBranchDirectory);
 		assertEquals("branch-a", output.trim());
 	}
 	@Test
