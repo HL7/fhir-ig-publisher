b446153d2691584223764bad0fd3f6d004ea259e
dotasek <david.otasek@smilecdr.com>
2023-03-14 09:26:03 -0400

    More error reporting

--- org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/GitUtilities.java
+++ org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/GitUtilities.java
@@ -1,17 +1,25 @@
 package org.hl7.fhir.igtools.publisher;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class GitUtilities {
 
 	protected static String execAndReturnString(String[] cmd, String[] env, File directory) throws IOException, InterruptedException {
-		String output = "";
+
 		Process p = Runtime.getRuntime().exec(cmd, null, directory);
-		InputStreamReader isr = new InputStreamReader(p.getInputStream());
-		p.waitFor();
+		int result = p.waitFor();
+
+		if (result == 0) {
+			return toString(p.getInputStream());
+		}
+
+		String error = toString(p.getErrorStream());
+		throw new RuntimeException(error);
+	}
+
+	private static String toString(InputStream inputStream) throws InterruptedException, IOException {
+		String output = "";
+		InputStreamReader isr = new InputStreamReader(inputStream);
 		BufferedReader br = new BufferedReader(isr);
 		String line;
 		while ((line = br.readLine()) != null) {
@@ -19,6 +27,7 @@ public class GitUtilities {
 		}
 		return output;
 	}
+
 	public static String getGitStatus(File gitDir) {
 	  if (!gitDir.exists()) {
 		return "";
