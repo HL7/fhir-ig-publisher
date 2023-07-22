cee70f7c8cbbb55dbe45d23ccca645e1604111ee
Grahame Grieve <grahameg@gmail.ccom>
2023-07-22 22:04:24 +1000

    add qa.txt for packages (for consistency)

--- org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/Publisher.java
+++ org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/Publisher.java
@@ -975,9 +975,13 @@ public class Publisher implements IWorkerContext.ILoggingService, IReferenceReso
     Utilities.createDirectory(outputDir);
     long startTime = System.nanoTime();
     JsonObject qaJson = new JsonObject();
+    StringBuilder txt = new StringBuilder(); 
     qaJson.add("url", templateInfo.asString("canonical"));
+    txt.append("url = "+templateInfo.asString("canonical")+"\r\n");
     qaJson.add("package-id", templateInfo.asString("name"));
+    txt.append("package-id = "+templateInfo.asString("name")+"\r\n");
     qaJson.add("ig-ver", templateInfo.asString("version"));
+    txt.append("ig-ver = "+templateInfo.asString("version")+"\r\n");
     qaJson.add("date", new SimpleDateFormat("EEE, dd MMM, yyyy HH:mm:ss Z", new Locale("en", "US")).format(execTime.getTime()));
     qaJson.add("version", Constants.VERSION);
     qaJson.add("tool", Constants.VERSION+" ("+ToolsVersion.TOOLS_VERSION+")");
@@ -999,10 +1003,12 @@ public class Publisher implements IWorkerContext.ILoggingService, IReferenceReso
     } catch (Exception e) {
       e.printStackTrace();
       qaJson.add("exception", e.getMessage());
+      txt.append("exception = "+e.getMessage()+"\r\n");
     }
     long endTime = System.nanoTime();
     String json = org.hl7.fhir.utilities.json.parser.JsonParser.compose(qaJson, true);
     TextFile.stringToFile(json, Utilities.path(outputDir, "qa.json"), false);
+    TextFile.stringToFile(txt.toString(), Utilities.path(outputDir, "qa.txt"), false);
 
     Utilities.createDirectory(tempDir);
     ZipGenerator zip = new ZipGenerator(Utilities.path(tempDir, "full-ig.zip"));
