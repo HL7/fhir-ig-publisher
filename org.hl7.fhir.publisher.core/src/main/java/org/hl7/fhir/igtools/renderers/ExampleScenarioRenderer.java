fc652e452c965b0be444508e3aae05312d8abb0a
Lloyd McKenzie <lloyd@lmckenzie.com>
2023-08-01 09:40:45 -0600

    Added support for ExampleScenario rendering

---
+++ org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/renderers/ExampleScenarioRenderer.java
@@ -0,0 +1,68 @@
+package org.hl7.fhir.igtools.renderers;
+
+/*-
+ * #%L
+ * org.hl7.fhir.publisher.core
+ * %%
+ * Copyright (C) 2014 - 2019 Health Level 7
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import org.hl7.fhir.exceptions.DefinitionException;
+import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.exceptions.FHIRFormatError;
+import org.hl7.fhir.igtools.publisher.IGKnowledgeProvider;
+import org.hl7.fhir.igtools.publisher.SpecMapManager;
+import org.hl7.fhir.r5.context.IWorkerContext;
+import org.hl7.fhir.r5.model.ExampleScenario;
+import org.hl7.fhir.r5.renderers.utils.RenderingContext;
+import org.hl7.fhir.r5.renderers.utils.RenderingContext.ExampleScenarioRendererMode;
+import org.hl7.fhir.r5.utils.EOperationOutcome;
+import org.hl7.fhir.utilities.MarkDownProcessor;
+import org.hl7.fhir.utilities.npm.NpmPackage;
+import org.hl7.fhir.utilities.xhtml.XhtmlComposer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+public class ExampleScenarioRenderer extends CanonicalRenderer {
+
+
+  private ExampleScenario scen;
+  private String destDir;
+
+  public ExampleScenarioRenderer(IWorkerContext context, String corePath, ExampleScenario scen, String destDir, IGKnowledgeProvider igp, List<SpecMapManager> maps, Set<String> allTargets, MarkDownProcessor markdownEngine, NpmPackage packge, RenderingContext gen) {
+    super(context, corePath, scen, destDir, igp, maps, allTargets, markdownEngine, packge, gen);
+    this.scen = scen;
+    this.destDir = destDir;
+  }
+
+  @Override
+  protected void genSummaryRowsSpecific(StringBuilder b, Set<String> rows) {
+  }
+
+  public String render(ExampleScenarioRendererMode mode) throws IOException, FHIRFormatError, DefinitionException, FHIRException, EOperationOutcome {
+    org.hl7.fhir.r5.renderers.ExampleScenarioRenderer sr = new org.hl7.fhir.r5.renderers.ExampleScenarioRenderer(gen);
+    gen.setScenarioMode(mode);
+    return new XhtmlComposer(XhtmlComposer.HTML).compose(sr.build(scen));
+  }
+
+  public String renderDiagram() throws IOException, FHIRFormatError, DefinitionException, FHIRException, EOperationOutcome {
+    org.hl7.fhir.r5.renderers.ExampleScenarioRenderer sr = new org.hl7.fhir.r5.renderers.ExampleScenarioRenderer(gen);
+    return sr.renderDiagram(scen);
+  }
+}
