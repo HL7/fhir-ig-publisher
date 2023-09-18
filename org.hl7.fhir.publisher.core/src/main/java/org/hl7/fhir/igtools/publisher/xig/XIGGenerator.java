39104259f4e2deeb5738b5e9d41bdcf53887d5da
Grahame Grieve <grahameg@gmail.ccom>
2023-09-18 17:05:17 +1000

    rework the XIG builder

--- org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/xig/XIGGenerator.java
+++ org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/xig/XIGGenerator.java
@@ -1,5 +1,6 @@
 package org.hl7.fhir.igtools.publisher.xig;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -32,8 +33,6 @@ public class XIGGenerator {
 
   private String target;
   private String cache;
-  private XIGInformation info = new XIGInformation();
-
   private FilesystemPackageCacheManager pcm;
 
   private String date;
@@ -48,195 +47,52 @@ public class XIGGenerator {
     this.cache = cache;
     Utilities.createDirectory(cache);
     pcm = new FilesystemPackageCacheManager(org.hl7.fhir.utilities.npm.FilesystemPackageCacheManager.FilesystemPackageCacheMode.USER);
-    NpmPackage npm = pcm.loadPackage("hl7.fhir.r5.core#5.0.0");
-    info.setCtxt(new SimpleWorkerContext.SimpleWorkerContextBuilder().fromPackage(npm, new PublisherLoader(npm, SpecMapManager.fromPackage(npm), npm.getWebLocation(), null).makeLoader(), false));
-    info.getCtxt().setAllowLazyLoading(false);
-    info.getCtxt().setAllowLoadingDuplicates(true);
-//    this.ctxt.connectToTSServer(TerminologyClientFactory.makeClient(Servers., "fhir/publisher", FhirPublication.R5), null);
-    info.getCtxt().setExpansionProfile(buildExpansionProfile());
     String ds = new SimpleDateFormat("dd MMM yyyy", new Locale("en", "US")).format(Calendar.getInstance().getTime());
     String dl = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssZ", new Locale("en", "US")).format(Calendar.getInstance().getTime());
-    date = "<span title=\""+dl+"\">"+ds+"</span>";
-    
-    info.getJson().add("date", date);
-    JsonObject doco = new JsonObject();
-    info.getJson().add("_doco", doco);
-    doco.add("packages", "An object containing of all the packages found, with {id}#{version} : package.json contents");
-    doco.add("canonicals", "An array of all the canonical resources found, with details");
-    addDoco(doco); 
-    
-    info.getJson().add("packages", new JsonObject());
-    info.getJson().add("canonicals", new JsonArray()); 
-  }
-
-  private void addDoco(JsonObject doco) {
-    JsonObject docoC = new JsonObject();
-    doco.add("canonicalDetails", docoC);
-    docoC.add("pid", "package where this resource was found");
-    docoC.add("fver", "version of FHIR for this resource");
-    docoC.add("published", "true if this is from a published package (as oppoosed to the ci-build");
-    docoC.add("filebase", "the name of the file (same directory as this json file). Both .json and .html exist");
-    docoC.add("path", "Where to find the HTML publication of this resource");
-    docoC.add("resourceType", "the type of this resource");
-    docoC.add("id", "id of the resource in the source package"); 
-    docoC.add("canonical", "canonical URL, if there is one"); 
-    docoC.add("version", "business version, if there is one"); 
-    docoC.add("status", "status, if there is one"); 
-    docoC.add("publisher", "stated publisher, if there is one"); 
-    docoC.add("name", "name, if there is one"); 
-    docoC.add("title", "title, if there is one"); 
-    docoC.add("date", "stated date, if there is one"); 
-    docoC.add("experimental", "whether the resource is marked experimental"); 
-    docoC.add("description", "description, if there is one");
-    docoC.add("copyright", "stated copyright, if there is one"); 
-    docoC.add("jurisdiction", "stated jurisdiction, if there is one"); 
-        
-
-    docoC.add("abstract", "abstract, if stated (StructureDefinition)"); 
-    docoC.add("affectsState", "affectsState, if stated (OperationDefinition)"); 
-    docoC.add("base", "base, if stated (OperationDefinition, StructureDefinition)"); 
-    docoC.add("caseSensitive", "caseSensitive, if stated (CodeSystem)"); 
-    docoC.add("code", "code, if stated (OperationDefinition, SearchParameter)"); 
-    docoC.add("compositional", "compositional, if stated (CodeSystem)"); 
-    docoC.add("content", "content, if stated (CodeSystem)"); 
-    docoC.add("contextInvs", "array of contextInvs in the resource (StructureDefinition)"); 
-    docoC.add("contexts", "array of contexts (type:expression in the resource (StructureDefinition)"); 
-    docoC.add("derivation", "derivation, if stated (StructureDefinition)"); 
-    docoC.add("fhirVersion", "Stated version in the resource (CapabilityStatement, StructureDefinition)"); 
-    docoC.add("formats", "array of formats and patchFormats in the resource (CapabilityStatement)"); 
-    docoC.add("hierarchyMeaning", "hierarchyMeaning, if stated (CodeSystem)"); 
-    docoC.add("immutable", "immutable, if stated (ValueSet)"); 
-    docoC.add("implementationGuides", "array of implementationGuides in the resource (CapabilityStatement)"); 
-    docoC.add("imports", "array of imports  in the resource (CapabilityStatement)"); 
-    docoC.add("instance", "instance, if stated (OperationDefinition)"); 
-    docoC.add("instantiates", "array of instantiates claims in the resource (CapabilityStatement)"); 
-    docoC.add("keywords", "array of keywords in the resource (StructureDefinition)"); 
-    docoC.add("kind", "kind, if stated (CapabilityStatement, NamingSystem, StructureDefinition)"); 
-    docoC.add("languages", "array of languages in the resource (CapabilityStatement)"); 
-    docoC.add("resources", "array of base resource, in the resource (OperationDefinition)"); 
-    docoC.add("resourcesSP", "array of base resource, in the resource (SearchParameter)"); 
-    docoC.add("sourceScope", "sourceScope, if stated (ConceptMap"); 
-    docoC.add("sources", "array of sources in the resource (ConceptMap"); 
-    docoC.add("supplements", "supplements, if stated (CodeSystem)"); 
-    docoC.add("system", "system, if stated (OperationDefinition)"); 
-    docoC.add("targetScope", "targetScope, if stated (ConceptMap"); 
-    docoC.add("targets", "array of targets in the resource (ConceptMap"); 
-    docoC.add("type", "type, if stated (NamingSystem, OperationDefinition, SearchParameter, StructureDefinition)"); 
-    docoC.add("valueSet", "valueSet, if stated (CodeSystem)"); 
-    docoC.add("versionNeeded", "versionNeeded, if stated (CodeSystem)");
-  }
-
-  private Parameters buildExpansionProfile() {
-    Parameters res = new Parameters();
-    res.addParameter("profile-url", "urn:uuid:"+UUID.randomUUID().toString().toLowerCase());
-    res.addParameter("excludeNested", false);
-    res.addParameter("includeDesignations", true);
-    // res.addParameter("activeOnly", true);
-    res.addParameter("system-version", "http://snomed.info/sct|http://snomed.info/sct/"+SNOMED_EDITION); // value sets are allowed to override this. for now
-    return res;
+    date = "<span title=\""+dl+"\">"+ds+"</span>";    
   }
   
   public void execute(String step) throws IOException, ParserConfigurationException, SAXException, FHIRException, EOperationOutcome, ParseException {
     ProfileUtilities.setSuppressIgnorableExceptions(true);
-    
-    if (step.equals("step1")) {
-      long ms = System.currentTimeMillis();
-      
-      Utilities.clearDirectory(target);
-      PackageVisitor pv = new PackageVisitor();
-      pv.getResourceTypes().add("CapabilityStatement");
-      pv.getResourceTypes().add("SearchParameter");
-      pv.getResourceTypes().add("OperationDefinition");
-      pv.getResourceTypes().add("StructureDefinition");
-      pv.getResourceTypes().add("ValueSet");
-      pv.getResourceTypes().add("CodeSystem");
-      pv.getResourceTypes().add("ConceptMap");
-      pv.getResourceTypes().add("StructureMap");
-      pv.getResourceTypes().add("NamingSystem");
-      pv.getResourceTypes().add("GraphDefinition");
-      pv.getResourceTypes().add("ActivityDefinition");
-      pv.getResourceTypes().add("ConditionDefinition");
-      pv.getResourceTypes().add("DeviceDefinition");
-      pv.getResourceTypes().add("EventDefinition");
-      pv.getResourceTypes().add("ObservationDefinition");
-      pv.getResourceTypes().add("PlanDefinition");
-      pv.getResourceTypes().add("Questionnaire");
-      pv.getResourceTypes().add("SpecimenDefinition");
-      pv.getResourceTypes().add("ExampleScenario");
-      pv.getResourceTypes().add("ActorDefinition");
-      pv.getResourceTypes().add("Requirements");
-      
-      pv.setCache(cache);
-      pv.setOldVersions(false);
-      pv.setCorePackages(false);
-      XIGGatherer gather = new XIGGatherer(target);
-      pv.setProcessor(gather);
-      pv.setCurrent(true);
-      pv.visitPackages();
-      gather.finish();
-      
-      System.out.println("Finished Step 1: "+Utilities.describeDuration(System.currentTimeMillis() - ms));
-    } else if  (step.equals("step2")) {
-      long ms = System.currentTimeMillis();
-      XIGLoader loader = new XIGLoader(info);
-      loader.loadFromCache(target);
-      System.out.println("Finished Step 2 A: "+Utilities.describeDuration(System.currentTimeMillis() - ms));
-      info.buildUsageMap();
-      System.out.println("Finished Step 2 B: "+Utilities.describeDuration(System.currentTimeMillis() - ms));
-      System.out.println("Finished Step 2 C: "+Utilities.describeDuration(System.currentTimeMillis() - ms));      printSummary(); 
-      new XIGRenderer(info, target, date).produce(pcm);
-      System.out.println("Finished Step 2 D: "+Utilities.describeDuration(System.currentTimeMillis() - ms));
-    } else {
-      PackageVisitor pv = new PackageVisitor();
-      pv.getResourceTypes().add("CapabilityStatement");
-      pv.getResourceTypes().add("SearchParameter");
-      pv.getResourceTypes().add("OperationDefinition");
-      pv.getResourceTypes().add("StructureDefinition");
-      pv.getResourceTypes().add("ValueSet");
-      pv.getResourceTypes().add("CodeSystem");
-      pv.getResourceTypes().add("ConceptMap");
-      pv.getResourceTypes().add("StructureMap");
-      pv.getResourceTypes().add("NamingSystem");
-      pv.getResourceTypes().add("GraphDefinition");
-      pv.getResourceTypes().add("ActivityDefinition");
-      pv.getResourceTypes().add("ConditionDefinition");
-      pv.getResourceTypes().add("DeviceDefinition");
-      pv.getResourceTypes().add("EventDefinition");
-      pv.getResourceTypes().add("ObservationDefinition");
-      pv.getResourceTypes().add("PlanDefinition");
-      pv.getResourceTypes().add("Questionnaire");
-      pv.getResourceTypes().add("SpecimenDefinition");
-      pv.getResourceTypes().add("ExampleScenario");
-      pv.getResourceTypes().add("ActorDefinition");
-      pv.getResourceTypes().add("Requirements");
 
-      pv.setOldVersions(false);
-      pv.setCorePackages(false);
-      pv.setProcessor(new XIGLoader(info));
-      pv.setCurrent(true);
-      pv.visitPackages();
+    long ms = System.currentTimeMillis();
 
-      info.buildUsageMap();
+    new File(target).delete();
 
-      new XIGRenderer(info, target, date).produce(pcm);
-      printSummary(); 
-    }
-  }
+    PackageVisitor pv = new PackageVisitor();
+    pv.getResourceTypes().add("CapabilityStatement");
+    pv.getResourceTypes().add("SearchParameter");
+    pv.getResourceTypes().add("OperationDefinition");
+    pv.getResourceTypes().add("StructureDefinition");
+    pv.getResourceTypes().add("ValueSet");
+    pv.getResourceTypes().add("CodeSystem");
+    pv.getResourceTypes().add("ConceptMap");
+    pv.getResourceTypes().add("StructureMap");
+    pv.getResourceTypes().add("NamingSystem");
+    pv.getResourceTypes().add("GraphDefinition");
+    pv.getResourceTypes().add("ActivityDefinition");
+    pv.getResourceTypes().add("ConditionDefinition");
+    pv.getResourceTypes().add("DeviceDefinition");
+    pv.getResourceTypes().add("EventDefinition");
+    pv.getResourceTypes().add("ObservationDefinition");
+    pv.getResourceTypes().add("PlanDefinition");
+    pv.getResourceTypes().add("Questionnaire");
+    pv.getResourceTypes().add("SpecimenDefinition");
+    pv.getResourceTypes().add("ExampleScenario");
+    pv.getResourceTypes().add("ActorDefinition");
+    pv.getResourceTypes().add("Requirements");
 
-  private void printSummary() throws FileNotFoundException, IOException {
-    info.getExtensionHandler().finish(target);
-    System.out.println("");
-    System.out.println("IGs: "+info.getPid().size());
-    int i = 0;
-    for (String s : Utilities.sorted(info.getCounts().keySet())) {
-      i = i + info.getCounts().get(s).size();
-      System.out.println(s+": "+info.getCounts().get(s).size());
-    }
-    System.out.println("Total: "+i);
-    System.out.println("SD Error List:");
-    for (String s : info.getSDErrors()) {
-      System.out.println("  "+s);
-    }
+    pv.setCache(cache);
+    pv.setOldVersions(false);
+    pv.setCorePackages(true);
+    XIGDatabaseBuilder gather = new XIGDatabaseBuilder(target, new SimpleDateFormat("dd MMM yyyy", new Locale("en", "US")).format(Calendar.getInstance().getTime()));
+    pv.setProcessor(gather);
+    pv.setCurrent(true);
+    pv.visitPackages();
+    gather.finish();
+
+    System.out.println("Finished Step 1: "+Utilities.describeDuration(System.currentTimeMillis() - ms));
+    System.out.println("File size is "+Utilities.describeSize(new File(target).length()));
   }
 
 }
