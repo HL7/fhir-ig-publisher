eb38552e44fb3e49a3cc0118f80d0ec56f6a5d0e
Grahame Grieve <grahameg@gmail.ccom>
2025-07-18 21:52:03 +1000

    add terminologies support in FHIRPath

--- org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/IGPublisherHostServices.java
+++ org.hl7.fhir.publisher.core/src/main/java/org/hl7/fhir/igtools/publisher/IGPublisherHostServices.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.NotImplementedException;
 import org.hl7.fhir.exceptions.FHIRException;
 import org.hl7.fhir.exceptions.PathEngineException;
 import org.hl7.fhir.r5.context.SimpleWorkerContext;
+import org.hl7.fhir.r5.fhirpath.BaseHostServices;
 import org.hl7.fhir.r5.fhirpath.FHIRPathEngine;
 import org.hl7.fhir.r5.fhirpath.TypeDetails;
 import org.hl7.fhir.r5.liquid.GlobalObject;
@@ -24,29 +25,22 @@ import org.hl7.fhir.r5.utils.validation.IResourceValidator;
 import org.hl7.fhir.utilities.Utilities;
 import org.hl7.fhir.utilities.validation.ValidationMessage;
 
-public class IGPublisherHostServices implements IEvaluationContext {
-
-
+public class IGPublisherHostServices extends BaseHostServices {
+  
   private IGKnowledgeProvider igpkp;
   private List<FetchedFile> fileList = new ArrayList<FetchedFile>();
   private SimpleWorkerContext context;
   private DateTimeType dt;
   private StringType pathToSpec;
-  private Map<String, FunctionDefinition> functions = new HashMap<>();
   
   public IGPublisherHostServices(IGKnowledgeProvider igpkp, List<FetchedFile> fileList,
       SimpleWorkerContext context, DateTimeType dt, StringType pathToSpec) {
-    super();
+    super(context);
     this.igpkp = igpkp;
     this.fileList = fileList;
     this.context = context;
     this.dt = dt;
     this.pathToSpec = pathToSpec;
-  }
-
-  public IGPublisherHostServices registerFunction(FunctionDefinition function) {
-    functions.put(function.name(), function);
-    return this;
   }
   
   @Override
@@ -77,24 +71,6 @@ public class IGPublisherHostServices implements IEvaluationContext {
   @Override
   public boolean log(String argument, List<Base> focus) {
     return false;
-  }
-
-  @Override
-  public FunctionDetails resolveFunction(FHIRPathEngine engine, String functionName) {
-    FunctionDefinition fd = functions.get(functionName);
-    return fd == null ? null : fd.details();
-  }
-
-  @Override
-  public TypeDetails checkFunction(FHIRPathEngine engine, Object appContext, String functionName, TypeDetails focus, List<TypeDetails> parameters) throws PathEngineException {
-    FunctionDefinition fd = functions.get(functionName);
-    return fd == null ? null : fd.check(engine, appContext, focus, parameters);
-  }
-
-  @Override
-  public List<Base> executeFunction(FHIRPathEngine engine, Object appContext, List<Base> focus, String functionName, List<List<Base>> parameters) {
-    FunctionDefinition fd = functions.get(functionName);
-    return fd == null ? null : fd.execute(engine, appContext, focus, parameters);
   }
 
   @Override
